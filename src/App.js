5b6d56bfb3db570a7cfd67516426d21b39b03cac
ellisrugazoora <ellisrgrz@gmail.com>
2024-02-01 05:52:06 -0800

    imported logout!

--- src/App.js
+++ src/App.js
@@ -6,13 +6,25 @@ import StockApp from "./Components/StockApp";
 import Music from './Components/Music/Music';
 
 import { Box, ChakraBaseProvider, ChakraProvider } from '@chakra-ui/react';
-import { ensureIsUser, initThinBackend } from 'thin-backend';
+import { ensureIsUser, initThinBackend, logout } from 'thin-backend';
 import { ThinBackend } from 'thin-backend-react';
 
 initThinBackend({
   // This url is different for each backend, this one points to 'mogaslobpmrp'
   host: 'https://mogaslobpmrp.thinbackend.app'
 });
+
+function UserStatus() {
+  // Use the `useCurrentUser()` react hook to access the current logged in user
+  // Returns `null` while the user is being fetched
+  const user = useCurrentUser();
+
+  return <div>
+      {user?.email}
+
+      <button onClick={logout}>Logout</button>
+  </div>
+}
 
 function App() {
   var display = {a: <StockApp />, b: <Tanga />, c: <Music />}
