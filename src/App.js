66bf026a2b179ff5aafae9fbec064fe819df0e7a
ellisrugazoora <ellisrgrz@gmail.com>
2024-02-01 05:43:51 -0800

    added require login

--- src/App.js
+++ src/App.js
@@ -6,17 +6,25 @@ import StockApp from "./Components/StockApp";
 import Music from './Components/Music/Music';
 
 import { Box, ChakraBaseProvider, ChakraProvider } from '@chakra-ui/react';
+import { ensureIsUser, initThinBackend } from 'thin-backend';
+import { ThinBackend } from 'thin-backend-react';
+
+initThinBackend({
+  // This url is different for each backend, this one points to 'mogaslobpmrp'
+  host: 'https://mogaslobpmrp.thinbackend.app'
+});
 
 function App() {
   var display = {a: <StockApp />, b: <Tanga />, c: <Music />}
   return (
-    <ChakraProvider>
+    <ThinBackend requireLogin>
+      <ChakraProvider>
       {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /> */}
-      <Box className="App" width={"100%"}>
+      {/* <Box className="App" width={"100%"}>
         {display.b}
-      </Box>
-      
-    </ChakraProvider>
+      </Box> */}
+      </ChakraProvider>
+    </ThinBackend>
   );
 }
 
