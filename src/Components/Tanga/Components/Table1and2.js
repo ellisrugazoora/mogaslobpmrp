3efe579831a9a77c5854f93cbff897c65dd5debc
ellisrugazoora <ellisrgrz@gmail.com>
2024-02-05 03:37:40 -0800

    Updated financial plan

--- src/Components/Tanga/Components/Table1and2.js
+++ src/Components/Tanga/Components/Table1and2.js
@@ -6,55 +6,80 @@ import "ag-grid-community/styles/ag-grid.css"; // Core CSS
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Theme
 import TableAG from "./TableAG";
 
-var columns = ["Consignment", "Date1", "Date2", "Date3"]
+var columns = ["Consignment", "Date1", "Date2", "Date3"];
 
 
 
 function Table1and2(props){
-    const [rawdata, SetRawData] = useState(() => {
+    const [data, SetData] = useState(() => {
         let stored = localStorage.getItem(props.title + "_inv_table");
         return stored ? JSON.parse(stored) : {
-            inv1: {col1: "500SN/600N", col2: 10, hs: 20, col4: 60, order: 30, col5: 40},
-            inv2: {col1: "150SN", col2: 10, hs: 20,col4: 60,order: 30,col5: 40},
-            inv3: {col1: "BS150", col2: 10, hs: 20,col4: 60,order: 30,col5: 40},
-            inv4: {col1: "SN80/SN100", col2: 10, hs: 20,col4: 60,order: 30,col5: 40},
-            inv5: {col1: "DPK", col2: 10, hs: 20,col4: 60,order: 30,col5: 40},
+            inv1: {product: "500SN/600N", Qty: 10, Stockholding_period: 20, Leadtime: 60, orderdate: 30},
+            inv2: {product: "150SN", Qty: 10, Stockholding_period: 20,Leadtime: 60,orderdate: 30},
+            inv3: {product: "BS150", Qty: 10, Stockholding_period: 20,Leadtime: 60,orderdate: 30},
+            inv4: {product: "SN80/SN100", Qty: 10, Stockholding_period: 20,Leadtime: 60,orderdate: 30},
+            inv5: {product: "DPK", Qty: 10, Stockholding_period: 20,Leadtime: 60,orderdate: 30},
     
-            inv6: {col1: "TBN+", col2: 10, hs: 20,col4: 40,order: 30,col5: 40},
-            inv7: {col1: "PPD", col2: 10, hs: 20,col4: 40,order: 30,col5: 40},
-            inv8: {col1: "CI-4", col2: 10, hs: 20,col4: 40, order: 30,col5: 40},
-            inv9: {col1: "BS200", col2: 10, hs: 20,col4: 40,order: 30,col5: 40},
-            inv10: {col1: "VII", col2: 10, hs: 20,col4: 40,order: 30,col5: 40},
-            inv11: {col1: "MONO PA EO", col2: 10, hs: 20,col4: 40,order: 30,col5: 40},
-            inv12: {col1: "4T PA PEO", col2: 10, hs: 20,col4: 40,order: 30,col5: 40},
-            inv13:{col1: "ATF PA", col2: 10, hs: 20,col4: 40,order: 30, col5: 40},
-            inv14:{col1: "2T PA", col2: 10, hs: 20,col4: 40,order: 30, col5: 40},
-            inv15:{col1: "HYA", col2: 10, hs: 20,col4: 40,order: 30,col5: 40},
-            inv16:{col1: "DYE", col2: 10, hs: 20,col4: 40,order: 30,col5: 40},
-            inv17:{col1: "TURB", col2: 10, hs: 20,col4: 40,order: 30,col5: 40}
+            inv6: {product: "TBN+", Qty: 10, Stockholding_period: 20,Leadtime: 40,orderdate: 30},
+            inv7: {product: "PPD", Qty: 10, Stockholding_period: 20,Leadtime: 40,orderdate: 30},
+            inv8: {product: "CI-4", Qty: 10, Stockholding_period: 20,Leadtime: 40, orderdate: 30},
+            inv9: {product: "BS200", Qty: 10, Stockholding_period: 20,Leadtime: 40,orderdate: 30},
+            inv10:{product: "VII", Qty: 10, Stockholding_period: 20,Leadtime: 40,orderdate: 30},
+            inv11:{product: "MONO PA EO", Qty: 10, Stockholding_period: 20,Leadtime: 40,orderdate: 30},
+            inv12:{product: "4T PA PEO", Qty: 10, Stockholding_period: 20,Leadtime: 40,orderdate: 30},
+            inv13:{product: "ATF PA", Qty: 10, Stockholding_period: 20,Leadtime: 40,orderdate: 30},
+            inv14:{product: "2T PA", Qty: 10, Stockholding_period: 20,Leadtime: 40,orderdate: 30},
+            inv15:{product: "HYA", Qty: 10, Stockholding_period: 20,Leadtime: 40,orderdate: 30},
+            inv16:{product: "DYE", Qty: 10, Stockholding_period: 20,Leadtime: 40,orderdate: 30},
+            inv17:{product: "TURB", Qty: 10, Stockholding_period: 20,Leadtime: 40,orderdate: 30}
         }
     })
-    const [rowData, setRowData] = useState([
-        { make: "Tesla", model: "Model Y", price: 64950, electric: true },
-        { make: "Ford", model: "F-Series", price: 33850, electric: false },
-        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
-      ]);
+    const [rowData, setRowData] = useState(() => {
+        let data_array = Object.entries(data);
+        let rowdata_array = [];
+        data_array.forEach((value, index) => {
+            //testobject[value[0]] = value[1];
+            rowdata_array[index] = value[1];
+        })
+        return rowdata_array;
+        // [
+        // { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+        // { make: "Ford", model: "F-Series", price: 33850, electric: false },
+        // { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+        // ]
+    });
       
       // Column Definitions: Defines & controls grid columns.
-      const [colDefs, setColDefs] = useState([
-        { field: "make" },
-        { field: "model" },
-        { field: "price" },
-        { field: "electric" }
-      ]);
+      const [colDefs, setColDefs] = useState(()=>{
+        let data_array = Object.entries(data);
+        console.log(data_array)
+        let rowdata_array = [];
+        let columns_array = [];
+        data_array.forEach((value, index) => {
+            //testobject[value[0]] = value[1];
+            rowdata_array[index] = value[1];
+        })
+        let cols = Object.entries(rowdata_array[0]);
+        cols.forEach((value, index)=>{
+            columns_array.push({field: value[0]});
+        }) 
+
+        return columns_array
+        // [
+        // { field: "make" },
+        // { field: "model" },
+        // { field: "price" },
+        // { field: "electric" }
+        // ]
+    });
     var datafn = (month) => {
         console.log(month)
         //let id = month + "_inv_table";
-        //let rawdata = localStorage.getItem(id); 
-        //let jsondata = rawdata;
-        //console.log(`Json data: ${jsondata.inv1.col2}; raw data: ${rawdata}`)
+        //let data = localStorage.getItem(id); 
+        //let jsondata = data;
+        //console.log(`Json data: ${jsondata.inv1.col2}; raw data: ${data}`)
         return {
-            row1: {consignment: "Consignment1", date1: rawdata.inv1.col2, date2:"order", date3: "pay"},
+            row1: {consignment: "Consignment1", date1: data.inv1.col2, date2:"order", date3: "pay"},
             row2: {consignment: "Consignment1", date1:" ", date2:"order", date3: "pay"},
             row3: {consignment: "Consignment1", date1:" ", date2:"order", date3: "pay"},
             row4: {consignment: "Consignment1", date1:" ", date2:"order", date3: "pay"}
@@ -62,43 +87,60 @@ function Table1and2(props){
     }
     function handlerefresh(){
         if(localStorage.getItem(props.title + "_inv_table")){
-            SetRawData(JSON.parse(localStorage.getItem(props.title + "_inv_table")));
+            SetData(JSON.parse(localStorage.getItem(props.title + "_inv_table")));
         }
         else {
             console.log("There exists no local storage currrently")
         }
-    }
-    function handlePrintData(){
-        let dataobject = rawdata;
-        let dataarray = Object.entries(dataobject);
+        let dataarray = Object.entries(data);
         console.log(dataarray)
-        let testobject = {};
         let testarray = [];
         let testcolumnarray = [];
         dataarray.forEach((value, index) => {
-            testobject[value[0]] = value[1];
+            //testobject[value[0]] = value[1];
             testarray[index] = value[1];
         })
         let cols = Object.entries(testarray[0]);
         cols.forEach((value, index)=>{
-            testcolumnarray = value[0];
+            testcolumnarray.push({field: value[0]});
         }) 
-        console.log(testobject);
-        console.log(testarray);
-        console.log(cols);
+        //console.log(testobject);
+        console.log(testarray); //This is the row data
+        console.log(testcolumnarray); //this is the columns
+        setRowData(testarray);
+        setColDefs(testcolumnarray);
     }
+    // function handlePrintData(){
+    //     let dataarray = Object.entries(data);
+    //     console.log(dataarray)
+    //     let testarray = [];
+    //     let testcolumnarray = [];
+    //     dataarray.forEach((value, index) => {
+    //         //testobject[value[0]] = value[1];
+    //         testarray[index] = value[1];
+    //     })
+    //     let cols = Object.entries(testarray[0]);
+    //     cols.forEach((value, index)=>{
+    //         testcolumnarray.push({field: value[0]});
+    //     }) 
+    //     //console.log(testobject);
+    //     console.log(testarray); //This is the row data
+    //     console.log(testcolumnarray); //this is the columns
+    //     setRowData(testarray);
+    //     setColDefs(testcolumnarray);
+    // }
 
     return (
         <div>
             <div> 
             {/* <TaBle title="Payment schedule" columns={columns} data={data} />  */}
             <Button onClick={handlerefresh}>Refresh</Button>
-            <Button onClick={handlePrintData}>Print data</Button>
+            {/* <Button onClick={handlePrintData}>Print data</Button> */}
             {/* <AgGridReact rowData={rowData} columnDefs={colDefs} /> */}
-            <Center><TaBle title={props.title + " financing plan"} columns={columns} data={datafn(props.title)} /></Center>
-            Replace with AG charts! Below Anyone home?
+            {/* <Center><TaBle title={props.title + " financing plan"} columns={columns} data={datafn(props.title)} /></Center> */}
+            {/* Replace with AG charts! Below Anyone home? */}
             </div>
-            <div className="ag-theme-quartz" style={{ height: 300, width:700 }}>
+            <div className="ag-theme-quartz" style={{ height: 700, width:950 }} >
                 {/* The AG Grid component */}
                 <AgGridReact rowData={rowData} columnDefs={colDefs} />
             </div>
