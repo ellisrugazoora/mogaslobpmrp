add5589f4ec28cbd27f14f27761d7463826ec742
ellisrugazoora <ellisrgrz@gmail.com>
2024-02-20 08:15:20 -0800

    Delete unnecessary state declaration

--- src/Components/Tanga/Components/Formulas.js
+++ src/Components/Tanga/Components/Formulas.js
@@ -54,31 +54,7 @@ function Formulas(){
     function summer(obj){
         return obj._500SN + obj._150SN + obj._BS150 + obj._SN80 + obj.DPK + obj.TBN + obj.PPD + obj.CI4 + obj.GOA + obj.VII + obj.MONO_PA_EO + obj._4T_PA_PEO + obj.ATF_PA + obj._2T_PA + obj.HYA + obj.DYE + obj.Lubrizol8510;
     }
-    //initialize rowData as what is in the database
-    /*const [rowData, SetRowData] = useState(() => {
-        let stored_formula = localStorage.getItem('formulas')
-        return stored_formula ? JSON.parse(stored_formula) : [
-            {Product:"Sentry 4T", _500SN:0.8780, _150SN: 0, _BS150:0, _SN80:0, DPK:0, TBN:0.002,PPD:0.002, CI4:0, GOA:0,VII:0.0630,MONO_PA_EO:0, _4T_PA_PEO:0.0550, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Mogas 2T", _500SN:0.9050, _150SN: 0, _BS150:0, _SN80:0, DPK:0.08, TBN:0,PPD:0, CI4:0, GOA:0,VII:0,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0.015, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Duramax HD 40", _500SN:0.72, _150SN: 0, _BS150:0.23, _SN80:0, DPK:0, TBN:0.006,PPD:0, CI4:0, GOA:0,VII:0,MONO_PA_EO:0.0440, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Turbofleet 15W/40", _500SN:0.3950, _150SN: 0.3990, _BS150:0, _SN80:0, DPK:0, TBN:0.002,PPD:0.002, CI4:0.1170, GOA:0,VII:0.0850,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Sentry HD40", _500SN:0.73, _150SN: 0, _BS150:0.2330, _SN80:0, DPK:0, TBN:0.003,PPD:0, CI4:0, GOA:0,VII:0,MONO_PA_EO:0.0340, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Dynatrans 85W/140", _500SN:0.07, _150SN: 0, _BS150:0.8870, _SN80:0, DPK:0, TBN:0,PPD:0.0030, CI4:0, GOA:0.04,VII:0,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Hydrax Z 68", _500SN:0.6895, _150SN: 0.3, _BS150:0, _SN80:0, DPK:0, TBN:0,PPD:0.002, CI4:0, GOA:0,VII:0,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0.0085, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Duramax Extra 25W/50", _500SN:0.630, _150SN: 0, _BS150:0.2740, _SN80:0, DPK:0, TBN:0,PPD:0.002, CI4:0, GOA:0,VII:0.0500,MONO_PA_EO:0.0440, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Mogas SB22", _500SN:0, _150SN: 1, _BS150:0, _SN80:0, DPK:0, TBN:0,PPD:0, CI4:0, GOA:0,VII:0,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Dynatrans 80W/90", _500SN:0.7370, _150SN: 0, _BS150:0.2300, _SN80:0, DPK:0, TBN:0,PPD:0.002, CI4:0, GOA:0.0230,VII:0.0080,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Hydrax Z46", _500SN:0.2400, _150SN: 0.7495, _BS150:0, _SN80:0, DPK:0, TBN:0,PPD:0.002, CI4:0, GOA:0,VII:0,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0.0085, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Prestina T68", _500SN:0.6710, _150SN: 0.32, _BS150:0, _SN80:0, DPK:0, TBN:0,PPD:0.002, CI4:0, GOA:0,VII:0,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0.007, get Total(){return summer(this)}},
-            {Product:"Mogas ATF", _500SN:0, _150SN: 0.9317, _BS150:0, _SN80:0, DPK:0, TBN:0,PPD:0, CI4:0, GOA:0,VII:0,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0.0680, _2T_PA:0, HYA:0, DYE:0.0003, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Hydraz Z32", _500SN:0, _150SN: 0.9895, _BS150:0, _SN80:0, DPK:0, TBN:0,PPD:0.002, CI4:0, GOA:0,VII:0,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0.0085, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Frontia X 20W50", _500SN:0.6480, _150SN: 0.2160, _BS150:0, _SN80:0, DPK:0, TBN:0,PPD:0.002, CI4:0, GOA:0,VII:0.08,MONO_PA_EO:0, _4T_PA_PEO:0.0540, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            
-            {Product:"Powertrans SP150", _500SN:0.76, _150SN: 0, _BS150:0.2230, _SN80:0, DPK:0, TBN:0,PPD:0.002, CI4:0, GOA:0.0150,VII:0,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Powertrans SP220", _500SN:0.3280, _150SN: 0, _BS150:0.6500, _SN80:0, DPK:0, TBN:0,PPD:0.002, CI4:0, GOA:0.002,VII:0,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}},
-            {Product:"Powertrans SP320", _500SN:0.15, _150SN: 0, _BS150:0.8330, _SN80:0, DPK:0, TBN:0,PPD:0.002, CI4:0, GOA:0.0150,VII:0,MONO_PA_EO:0, _4T_PA_PEO:0, ATF_PA:0, _2T_PA:0, HYA:0, DYE:0, Lubrizol8510:0, get Total(){return summer(this)}}
-        ]    
-    })*/
+    
     var rows = [];
     var cols = [];
     rows = formulas.map((product, index)=>{
