e8bbd84ca9b0d62d175b9276c354efc812d4aed6
ellisrugazoora <ellisrgrz@gmail.com>
2024-02-28 00:49:32 -0800

    Added updateRecord for asof Date

--- src/Components/Tanga/Components/Packaging.js
+++ src/Components/Tanga/Components/Packaging.js
@@ -1,30 +1,69 @@
 import { Button, Center, Heading } from "@chakra-ui/react";
 import { AgGridReact } from "ag-grid-react";
-import { query } from "thin-backend";
+import { getCurrentUser, query, updateRecord } from "thin-backend";
 import { useQuery } from "thin-backend-react";
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css";
+import { useEffect, useState } from "react";
 
 function Packaging(){
     const packagingMaterials = useQuery(query('january_packaging_requirements'))
+    const [access, SetAcess] = useState({product: false, inventory: false, transit: false, consumption: false})
+    useEffect(()=>{
+        const fetchData = async () => {
+            let id = await getCurrentUser();
+            SetAcess({product: id.product, inventory: id.inventory, transit: id.transit, consumption: id.consumption})
+        }
+        fetchData()
+    },[])
+    
     if(packagingMaterials === null){
         return <div>Loading ...</div>
     }
     const rowData1 = packagingMaterials.map((value, index)=>{
-        return {Material: value.material, Quantity: value.quantity}
-        })
-        const colDefs1 = Object.entries(rowData1[0]).map((value, index)=>{
-        return {field: value[0], flex: 1}
-        })
+        return {Material: value.material, ["In stock"]: value.instock, ["As of"]: value.asof,
+            get "Holding period"(){
+                return "..."
+            },
+            get "Re order date"(){
+                return "..."
+            }
+        }
+    })
+    const idMap = {};
+    packagingMaterials.forEach((value, index)=>{
+        idMap[value.material]= value.id;
+    })
+    const colDefs1 = Object.entries(rowData1[0]).map((value, index)=>{
+        if(value[0] === "As of"){
+            return {field: value[0], flex: 1, editable:access.inventory, cellEditor: 'agDateStringCellEditor'}
+        }
+        else if (value[0] === "In stock"){
+            return {field: value[0], flex: 1, editable:access.inventory}
+        }
+        else {
+            return {field: value[0], flex: 1}
+        }
+    })
+    function saveChangedValue(e){
+        console.log(e, e.data)
+        let material = e.data.Material
+        let prop = e.column.colId
+        let new_val = e.newValue;
+        console.log(`Material: ${material}; id: ${idMap[material]} prop: ${prop}; new_val: ${new_val}`)
+        updateRecord('january_packaging_requirements',idMap[material],{asof: new_val})
+    }
+    
     return <div>
         Packaging test
-        <Button onClick={()=>{console.log(rowData1); console.log(colDefs1)}}>Print data</Button>
+        <Button onClick={()=>{console.log(rowData1); console.log(colDefs1); console.log(idMap)}}>Print data</Button>
         <Center>
-            <div className="ag-theme-quartz" style={{ height: 300, width:'50%', minWidth:340 }} >
-                <Heading fontSize={30}>Sales Projections</Heading>
+            <div className="ag-theme-quartz" style={{ height: 300, width:'80%', minWidth:340 }} >
+                <Heading fontSize={30}>Packaging inventory</Heading>
                 <AgGridReact 
                     rowData={rowData1} 
                     columnDefs={colDefs1}
+                    onCellValueChanged={saveChangedValue}
                     />
             </div>
         </Center>
